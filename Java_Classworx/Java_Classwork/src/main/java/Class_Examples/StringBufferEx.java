d72605af641a7c5ac5ee1c75d66473acb3e74afd
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-02 17:08:44 -0400

    String Buffer Excercise

---
+++ Java_Classworx/Java_Classwork/src/main/java/Class_Examples/StringBufferEx.java
@@ -0,0 +1,10 @@
+package Class_Examples;
+import java.lang.StringBuffer;
+
+public class StringBufferEx {
+   static StringBuffer sb = new StringBuffer("Buffer The Stringpire Slayer");
+    public static void main(String[] args) {
+    sb.append("!");
+        System.out.println(sb);
+    }
+}
