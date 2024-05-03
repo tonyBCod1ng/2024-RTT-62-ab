823099791770fea34131d1b39c82420c69758ebe
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-03 14:58:39 -0400

    looping through lists

--- Java_Classworx/Java_Classwork/src/main/java/Class_Examples/ForLoopDemo.java
+++ Java_Classworx/Java_Classwork/src/main/java/Class_Examples/ForLoopDemo.java
@@ -1,15 +1,34 @@
 package Class_Examples;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForLoopDemo {
     public static void main(String[] args) {
         forWhtItsWrth();
+        listaLaVista();
     }
+
     public static void forWhtItsWrth() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                System.out.println("Count is: " + i + "." +j);
+                System.out.println("Count is: " + i + "." + j);
             }
+        }
+    }
+    public static void listaLaVista() {
+        List<String> list = new ArrayList<>();
+        list.add("A");// pos 0
+        list.add("B");
+        list.add("C");
 
+        //old style of loop
+        for(int i = 0; i < list.size(); i--) {
+            System.out.println(list.get(i));
+        };
+        //new way to loop
+        for(String s : list) {
+            System.out.println(s);
         }
     }
 }
--- Java_Classworx/Java_Classwork/src/main/java/Java_Homework/PA_303_4_1/ControlFlowEx.java
+++ Java_Classworx/Java_Classwork/src/main/java/Java_Homework/PA_303_4_1/ControlFlowEx.java
@@ -2,12 +2,18 @@ package Java_Homework.PA_303_4_1;
 
 public class ControlFlowEx {
     public static void main(String[] args) {
-
+        lessThanTen();
     }
     //region - Write a program that declares 1 integer variable x, and then assigns 7 to it.
     // Write an if statement to print out “Less than 10” if x is less than 10.
     // Change x to equal 15, and notice the result (console should not display anything).
-
+        public static void lessThanTen() {
+         int x = 7;
+         x = 15; //causes condition below to become read false
+         if (x < 10) {
+             System.out.println("Less than 10");
+         }
+        }
     //endregion
 
     //region -Write a program that declares 1 integer variable x, and then assigns 7 to it.
