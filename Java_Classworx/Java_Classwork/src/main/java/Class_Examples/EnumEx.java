ae37d0037e75fa33ae3962a66cf0222f0876f0af
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-03 13:49:24 -0400

    Class Examples

---
+++ Java_Classworx/Java_Classwork/src/main/java/Class_Examples/EnumEx.java
@@ -0,0 +1,33 @@
+package Class_Examples;
+
+public class EnumEx {
+    public static void main(String[] args) {
+        Car car = Car.BMW;
+        switch (car) {
+            case BMW:
+                System.out.println("BMW");
+                break;
+            case AUDI:
+                System.out.println("Audi");
+                break;
+            case JEEP:
+                System.out.println("Jeep");
+                break;
+            case VOLKSWAGON:
+                System.out.println("Volkswagon");
+                break;
+            case NANO:
+                System.out.println("Nano");
+                break;
+            case FIAT:
+                System.out.println("Fiat");
+                break;
+            default:
+                System.out.println("Unknown");
+        }
+    }
+
+    enum Car {
+        BMW, AUDI, JEEP, VOLKSWAGON, NANO, FIAT
+    }
+}
---
+++ Java_Classworx/Java_Classwork/src/main/java/Class_Examples/SwitchGears.java
@@ -0,0 +1,27 @@
+package Class_Examples;
+
+import org.w3c.dom.ranges.Range;
+
+public class SwitchGears {
+    static int num = 3;
+    public static void main(String[] args) {
+        switchBlade();
+    }
+
+    public static void switchBlade() {
+
+        switch (num) {
+            case 0:
+                System.out.println("Bladerunner");
+                break;
+            case 1:
+                System.out.println("Switch foot");
+                break;
+            case 2:
+                System.out.println("Blade Trinity");
+                break;
+            default:
+                System.out.println("No blade found");
+        }
+    }
+}
