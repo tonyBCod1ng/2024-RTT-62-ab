1651c26c1fe9ba073da5d75e9b73ac0e37bf36f5
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-03 15:21:25 -0400

    while loop demo

---
+++ Java_Classworx/Java_Classwork/src/main/java/Class_Examples/WhileLoopDemo.java
@@ -0,0 +1,14 @@
+package Class_Examples;
+
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        whereTheWhileOnesAre();
+    }
+    public static void whereTheWhileOnesAre() {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
