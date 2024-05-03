536b81bf8bbe4add78f7408d32481d42917637bb
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-03 14:23:42 -0400

    added nested loop

--- Java_Classworx/Java_Classwork/src/main/java/Class_Examples/ForLoopDemo.java
+++ Java_Classworx/Java_Classwork/src/main/java/Class_Examples/ForLoopDemo.java
@@ -6,7 +6,10 @@ public class ForLoopDemo {
     }
     public static void forWhtItsWrth() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            for (int j = 0; j < 10; j++) {
+                System.out.println("Count is: " + i + "." +j);
+            }
+
         }
     }
 }
