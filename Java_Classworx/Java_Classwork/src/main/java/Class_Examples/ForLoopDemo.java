6cb34ef254856d1e1469dd2030f270854ed92f94
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-03 14:17:33 -0400

    for loop example

---
+++ Java_Classworx/Java_Classwork/src/main/java/Class_Examples/ForLoopDemo.java
@@ -0,0 +1,12 @@
+package Class_Examples;
+
+public class ForLoopDemo {
+    public static void main(String[] args) {
+        forWhtItsWrth();
+    }
+    public static void forWhtItsWrth() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
