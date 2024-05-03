02de24c2e53963ad8445613cc74ca043d17f528d
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-03 14:11:15 -0400

    HW setup

---
+++ Java_Classworx/Java_Classwork/src/main/java/Class_Examples/ControlFlowEx.java
@@ -0,0 +1,52 @@
+package Class_Examples;
+
+public class ControlFlowEx {
+    public static void main(String[] args) {
+
+    }
+    //region - Write a program that declares 1 integer variable x, and then assigns 7 to it.
+    // Write an if statement to print out “Less than 10” if x is less than 10.
+    // Change x to equal 15, and notice the result (console should not display anything).
+
+    //endregion
+
+    //region -Write a program that declares 1 integer variable x, and then assigns 7 to it.
+    // Write an if-else statement that prints out “Less than 10” if x is less than 10, and “Greater than 10”
+    // if x is greater than 10. Change x to 15 and notice the result.
+
+    //endregion
+
+    //region - Write a program that declares 1 integer variable x, and then assigns 15 to it.
+    // Write an if-else-if statement that prints out “Less than 10” if x is less than 10;
+    // “Between 10 and 20” if x is greater than 10 but less than 20,
+    // and “Greater than or equal to 20” if x is greater than or equal to 20.
+    // Change x to 50 and notice the result.
+
+    //endregion
+
+    //region -Write a program that declares 1 integer variable x, and then assigns 15 to it.
+    // Write an if-else statement that prints “Out of range” if the number is less than 10 or greater than 20
+    // and prints “In range” if the number is between 10 and 20 (including equal to 10 or 20).
+    // Change x to 5 and notice the result.
+
+    //endregion
+
+    //region -Write a program that uses if-else-if statements to print out grades A, B, C, D, F,
+    // according to the following criteria:
+    //A: 90-100
+    //B: 80-89
+    //C: 70-79
+    //D: 60-69
+    //F: <60
+    //Use the Scanner class to accept a number score from the user to determine the letter grade.
+    // Print out “Score out of range” if the score is less than 0 or greater than 100.
+
+    //endregion
+
+    //region -Write a program that accepts an integer between 1 and 7 from the user.
+    // Use a switch statement to print out the corresponding weekday.
+    // Print “Out of range” if the number is less than 1 or greater than 7.
+    // Do not forget to include “break” statements in each of your cases.
+
+    //endregion
+}
