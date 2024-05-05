e06f9d99afe9e6865c9ecc4f8da63c6f59644709
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-05 14:38:43 -0400

    completed loop HW -added GCD and Tuition prediction exercises

--- Java_Classwork/src/main/java/Java_Homework/PA_303_5_1/LoopHW.java
+++ Java_Classwork/src/main/java/Java_Homework/PA_303_5_1/LoopHW.java
@@ -1,9 +1,12 @@
 package Java_Homework.PA_303_5_1;
 
+import java.util.Scanner;
+
 public class LoopHW {
     public static void main(String[] args) {
         timeTable();
         daGreatest();
+        twoTimer();
     }
 
     public static void timeTable() {
@@ -15,9 +18,38 @@ public class LoopHW {
             leftSide++;
         }
     }
+
     public static void daGreatest() {
-        int a = 2;
-        int b = 4;
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1st number:");
+        int n1 = scanner.nextInt();
+        System.out.println("Enter 2nd number:");
+        int n2 = scanner.nextInt();
+        scanner.close();
+        int smll = Math.min(n1, n2);
+        int lrg = Math.max(n1, n2);
+        if(lrg % smll == 0) {
+            System.out.println("GCD is " + smll);
+            return;
+        }
+        while (lrg % smll != 0) {
+            int remainder = lrg % smll;
+            lrg = smll;
+            smll = remainder;
+        }
+        System.out.println("GCD is " + smll);
+    }
+    public static void twoTimer() {
+        int staringAmt = 10000;
+        int goalAmt = staringAmt * 2;
+        int yearsNeeded = 0;
+
+        while (staringAmt <= goalAmt) {
+            //takes last amount and add it to seven percent of itself
+            staringAmt += staringAmt * 0.07;
+            yearsNeeded++;
+        }
+
+        System.out.println("It will be " + yearsNeeded + " years before tuition doubles to " + staringAmt);
     }
 }
