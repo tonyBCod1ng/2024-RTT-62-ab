fc299e7f1ae91a6cff642d95857bf7439227ee1d
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-06 14:55:36 -0400

    Setting up business logic in coffee shop

--- Java_Classwork/src/main/java/Class_Examples/CoffeeShoppe/CoffeeShoppe.java
+++ Java_Classwork/src/main/java/Class_Examples/CoffeeShoppe/CoffeeShoppe.java
@@ -1,8 +1,30 @@
-package Class_Examples;
-import Class_Examples.CoffeeShoppe;
+package Class_Examples.CoffeeShoppe;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class CoffeeShoppe {
-
+  static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+       coffeeShoppe();
+    }
+    public static void coffeeShoppe() {
+        System.out.println("Welcome to Coffee Shopping System!");
+        System.out.println("What product do you want to add?");
+        String addedProduct = sc.nextLine();
+        System.out.println("What is the price of this item?");
+        double itemPrice = sc.nextDouble();
+        System.out.println("Is this a beverage item? 1 = yes, 2 = no");
+        boolean itemBeverage = sc.nextInt() == 1;
+        sc.close();
+        List<Product> products = new ArrayList<>();
+        Product newItem = new Product(addedProduct, itemPrice, itemBeverage);
 
+        products.add(newItem);
+        for (Product product : products) {
+            System.out.println(product.getName() + "....$" + String.format("%.2f", product.getPrice()));
+        }
     }
 }
