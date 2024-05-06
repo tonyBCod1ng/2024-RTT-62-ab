5c6db2d0db51767f5e91be0e0771bb27a7ca9ff9
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-06 13:55:05 -0400

    tinkering with array sort

--- Java_Classwork/src/main/java/Class_Examples/ArraySort.java
+++ Java_Classwork/src/main/java/Class_Examples/ArraySort.java
@@ -7,19 +7,24 @@ public class ArraySort {
         letsSortThisOut();
         }
         public static void letsSortThisOut() {
-            int[] array = {6, 0, 3, 5};
+            int[] array = {6, 0,-1, 3, 5};
 
-            for(int pos = 0; pos < array.length - 1; pos++) {
-                if(array[pos] <= 0 ) {
-                    array[pos] = array[0];
+            for(int outer = 0; outer < array.length; outer++) {
+            boolean swapped = false;
+                for(int pos = 0; pos < array.length - 1; pos++) {
+                
+                    if(array[pos] > array[pos+1]) {
+                        swapped = true;
+                        int temp = array[pos];
+                        array[pos] = array[pos+1];
+                        array[pos+1] = temp;
+                    }
+                    System.out.println(Arrays.toString(array));
+                    if(swapped){
+                        break;
+                    }
                 }
-                if(array[pos] > array[pos+1]) {
-                    int temp = array[pos];
-                    array[pos] = array[pos+1];
-                    array[pos+1] = temp;
-                }
-                System.out.println(Arrays.toString(array));
-        }
+            }
           System.out.println(Arrays.toString(array));
     }
 }
