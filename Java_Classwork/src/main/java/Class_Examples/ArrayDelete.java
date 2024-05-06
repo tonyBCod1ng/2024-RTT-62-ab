a25f6a9d07b427ff218597bf631696cd4d52cd4e
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-06 12:21:34 -0400

    refactored ArrayDeletion method

--- Java_Classwork/src/main/java/Class_Examples/ArrayDelete.java
+++ Java_Classwork/src/main/java/Class_Examples/ArrayDelete.java
@@ -2,21 +2,20 @@ package Class_Examples;
 
 public class ArrayDelete {
     public static void main(String[] args) {
-        deleteFunc(4);
+        deleteFunc(1);
     }
+
     public static void deleteFunc(int atPos) {
         String[] ogArray = {"A", "B", "C", "D", "E", "F"};
         String[] newArray = new String[ogArray.length - 1];
 
-
-        for(int i = 0; i < newArray.length; i++){
-
-            if(ogArray[i] == ogArray[atPos]){
-                newArray[i] = ogArray[i + 1];
-                continue;
-            }
+        for (int i = 0; i < newArray.length; i++) {
+            if (i < atPos) {
             newArray[i] = ogArray[i];
+            } else {
+                newArray[i] = ogArray[i + 1];
+            }
         }
-    System.out.println(newArray[atPos]);
+        System.out.println(newArray[atPos]);
     }
 }
