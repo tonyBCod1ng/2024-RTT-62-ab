5de4e9bdd8845e312a525b1471e166783bf63cd7
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-07 08:49:28 -0400

    housekeeping, removed TODO

--- Java_Classwork/src/main/java/Java_Homework/GLAB_303_2_1/ArithmeticOperatorsDemo.java
+++ Java_Classwork/src/main/java/Java_Homework/GLAB_303_2_1/ArithmeticOperatorsDemo.java
@@ -2,7 +2,7 @@ package Class_Examples.GLAB_303_2_1;
 
 public class ArithmeticOperatorsDemo {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+
         int x, y = 10, z = 5;
         x = y + z;
         System.out.println("+ operator resulted in " + x);
