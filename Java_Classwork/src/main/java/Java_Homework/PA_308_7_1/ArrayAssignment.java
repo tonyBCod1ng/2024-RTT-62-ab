599ccf820e0bf36775b8278fc823257e770b3ec2
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-07 09:44:26 -0400

    308_7_1 hw

---
+++ Java_Classwork/src/main/java/Java_Homework/PA_308_7_1/ArrayAssignment.java
@@ -0,0 +1,134 @@
+package Java_Homework.PA_308_7_1;
+
+import java.util.Arrays;
+
+public class ArrayAssignment {
+    public static void main(String[] args) {
+        arrayOfHope();
+        arraysToTheFinish();
+        arrayOfColor();
+        arrayLength();
+        arrayLoop();
+        arrayMultLoop();
+        skipMalcolm();
+        stringArray();
+        orderArray();
+    }
+
+    //region -Task 1: Write a program that creates an array of integers with a length of 3.
+    // Assign the values 1, 2, and 3 to the indexes. Print out each array element.
+    public static void arrayOfHope() {
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int item : arr) {
+            System.out.println(item);
+        }
+    }
+    //endregion
+
+    //region -Task 2: Write a program that returns the middle element in an array.
+    // Give the following values to the integer array: {13, 5, 7, 68, 2}
+    // and produce the following output: 7
+    public static void arraysToTheFinish() {
+        int[] arr = {13, 5, 7, 68, 2};
+
+        System.out.println(arr[2]);
+    }
+    //endregion
+
+    //region -Task 3: Write a program that creates an array of String type and
+    // initializes it with the strings “red,” “green,” “blue,” and “yellow.”
+    // Print out the array length. Make a copy using the clone( ) method.
+    // Use the Arrays.toString( ) method on the new array to verify that the original array
+    // was copied.
+    public static void arrayOfColor() {
+        String[] arr = {"red", "green", "blue", "yellow"};
+        System.out.println(arr.length);
+        String[] clonedArr = arr.clone();
+        System.out.println(Arrays.toString(clonedArr));
+    }
+    //endregion
+
+    //region -Write a program that creates an integer array with 5 elements (i.e., numbers).
+    // The numbers can be any integers.
+    // Print out the value at the first index and the last index using length - 1 as the index.
+    // Now try printing the value at index = length (e.g., myArray[myArray.length] ).
+    // Notice the type of exception which is produced.
+    // Now try to assign a value to the array index 5.
+    // You should get the same type of exception.
+    public static void arrayLength() {
+        int[] arr = new int[5];
+        System.out.println(arr[0]);
+        System.out.println(arr[arr.length - 1]);
+
+        //arr[5] = 6;//out of bounds
+    }
+    //endregion
+
+    //region -Write a program where you create an integer array with a length of 5.
+    // Loop through the array and assign the value of the loop control variable (e.g., i)
+    // to the corresponding index in the array.
+    public static void arrayLoop() {
+        int[] arr = new int[5];
+        for(int count = 0; count < arr.length; count++){
+            arr[count] = count;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    //endregion
+
+    //region - Write a program where you create an integer array of 5 numbers.
+    // Loop through the array and assign the value of the loop control variable
+    // multiplied by 2 to the corresponding index in the array.
+    public static void arrayMultLoop() {
+        int[] arr = new int[5];
+        for(int count = 0; count < arr.length; count++){
+            arr[count] = count * 2;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    //endregion
+
+    //region -Write a program where you create an array of 5 elements.
+    // Loop through the array and print the value of each element, except for the middle (index 2) element.
+    public static void skipMalcolm() {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        for(int count = 0; count < arr.length; count++){
+            if(count == 2){
+                continue;
+            }
+            System.out.println(arr[count]);
+        }
+    }
+    //endregion
+
+    //region -Write a program that creates a String array of 5 elements
+    // and swaps the first element with the middle element without creating a new array.
+    public static void stringArray() {
+        String[] arr = {"red", "green", "blue", "yellow"};
+        String temp;
+        temp = arr[0];
+        arr[0] = arr[2];
+        arr[2] = temp;
+        System.out.println(Arrays.toString(arr));
+    }
+    //endregion
+
+    //region -Write a program to sort the following int array in ascending order: {4, 2, 9, 13, 1, 0}.
+    // Print the array in ascending order, and print the smallest and the largest element of the array.
+    // The output will look like the following:
+    //Array in ascending order: 0, 1, 2, 4, 9, 13
+    //The smallest number is 0
+    //The biggest number is 13
+    public static void orderArray() {
+        int[] arr = {4, 2, 9, 13, 1, 0};
+        Arrays.sort(arr);
+        System.out.println("Ordered array is:" + Arrays.toString(arr));
+        System.out.println("Smallest number is: " + arr[0]);
+        System.out.println("Largest number is: " + arr[arr.length - 1]);
+    }
+    //endregion
+}
