1c7efd733caceb5bdeda44930456a8367cd8dd63
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-07 13:23:53 -0400

    Added functionality to ArrayDelete

--- Java_Classwork/src/main/java/Class_Examples/ArrayDelete.java
+++ Java_Classwork/src/main/java/Class_Examples/ArrayDelete.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 
  class ArrayDelete {
     public static Scanner scanner = new Scanner(System.in);
-
+    static String[] arr = {"A", "B", "C", "D", "E", "F"};
     public static void main(String[] args) {
         System.out.println("Enter the position of the item to delete: ");
         int position = scanner.nextInt();
-        miteDeleteLater(position);
+        miteDeleteLater(arr,position);
     }
 
-    public static void miteDeleteLater(int atPos) {
-        String[] ogArray = {"A", "B", "C", "D", "E", "F"};
+    public static void miteDeleteLater(String[] source, int atPos) {
+        String[] ogArray = source;
         String[] newArray = new String[ogArray.length - 1];
         if (atPos > ogArray.length - 1 || atPos < 0) {
             System.out.println("Position is out of bounds");
