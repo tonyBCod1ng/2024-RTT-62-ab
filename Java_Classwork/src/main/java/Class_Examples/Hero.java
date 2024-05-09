7b47ce46e724f8bd7367d50a170238a1a2dd7772
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-09 16:18:34 -0400

    tinkered with interface and implementation

--- Java_Classwork/src/main/java/Class_Examples/Hero.java
+++ Java_Classwork/src/main/java/Class_Examples/Hero.java
@@ -1,7 +1,14 @@
 package Class_Examples;
 
- class Hero {
+class Hero implements PerimeterInterface {
     public static void main(String[] args) {
+        Hero hero = new Hero();
+        hero.calculatePerimeter();
+    }
+
+    @Override
+    public double calculatePerimeter() {
         System.out.println("Hello World!");
+        return 0;
     }
 }
