38a35ee7e69d5ad3da532833b160701ba7180c58
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-10 18:29:49 -0400

    Java_Homework.GLAB_303_10_4

---
+++ Java_Classwork/src/main/java/Java_Homework/GLAB_303_10_4/Movable.java
@@ -0,0 +1,9 @@
+package Java_Homework.GLAB_303_10_4;
+
+public interface Movable {
+    void moveUp();    // "public" and "abstract" by default
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+    String getCoordinate();
+}
