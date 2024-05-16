32713c7915324afdf5bb527c6767a7314b240b3d
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-16 12:47:31 -0400

    hacker rank and class work

--- Java_Classwork/src/main/java/Class_Examples/FileExample.java
+++ Java_Classwork/src/main/java/Class_Examples/FileExample.java
@@ -3,8 +3,9 @@ package Class_Examples;
 import java.io.File;
 
 public class FileExample {
-    public static void main(String[] args) {
-        File file = new File("/Users/tonybcod1ng/Desktop/2024-RTT-62-ab/Java_Classwork/src/main/java/Class_Examples");
+   static final String PATH_NAME = "/Users/tonybcod1ng/Desktop/2024-RTT-62-ab/Java_Classwork/src/main/java/Class_Examples";
+    public static void main(String[] args) throws Exception {
+        File file = new File(PATH_NAME);
         File[] files = file.listFiles();
         for (File foundFile : files) {
             System.out.print(foundFile.getName());
@@ -14,5 +15,7 @@ public class FileExample {
                 System.out.println(" is a Directory");
             }
         }
+        File newFile = new File("./newFile.java");
+        newFile.createNewFile();
     }
 }
--- Java_Classwork/src/main/java/Java_Homework/PA_303_12_1.java
+++
@@ -1,5 +0,0 @@
-package Java_Homework;
-
-public class PA_303_12_1 {
-
-}
