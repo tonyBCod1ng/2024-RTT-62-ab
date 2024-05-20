e5aae6e28c865186f6a59ac32799d6b9273bc618
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-05-20 14:16:39 -0400

    added more code to KBA practice

--- Java_Classwork/src/main/java/Class_Examples/KBAPractice/Main.java
+++ Java_Classwork/src/main/java/Class_Examples/KBAPractice/Main.java
@@ -5,13 +5,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayToList arrayToList = new ArrayToList();
-        arrayToList.arrayList.add("1");
-        arrayToList.arrayList.add("2");
-        arrayToList.arrayList.add("3");
-        arrayToList.arrayList.add("4");
-        arrayToList.arrayList.add("5");
-        arrayToList.replaceWith(1);
-        arrayToList.replaceWith(3);
-        System.out.println(arrayToList.compact());
+       String[] arrayStuff = {"A", "B", "C", "D", "E", "F", "G", "H"};
+       arrayToList.convert(arrayStuff);
+       arrayToList.replaceWith(1);
+       arrayToList.replaceWith(3);
+       System.out.println(arrayToList.compact());
+
     }
 }
