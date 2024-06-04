97fd31c184741ce61e62802c0dd620bda4db58d5
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-06-04 19:09:33 -0400

    gitignore changes

--- Mod_305/src/main/java/ClassExamples/Database/Entity_and_DAO/JDBCDemo_CustDAO.java
+++ Mod_305/src/main/java/ClassExamples/Database/Entity_and_DAO/JDBCDemo_CustDAO.java
@@ -1,9 +1,8 @@
 package ClassExamples.Database.Entity_and_DAO;
-
+//comment
 
 import java.util.ArrayList;
 import java.util.List;
-
 class JDBCDemo_CustDAO {
      public static void main(String[] args) {
          CustomerDAO customerDAO = new CustomerDAO();
